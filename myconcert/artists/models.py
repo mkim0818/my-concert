f26c5fc72c42433c2c4aa655fbd677a87ade19c5
mkim0818 <mkim0818@gmail.com>
2016-02-25 13:22:46 +0900

    changed media folder name for amazon s3

--- myconcert/artists/models.py
+++ myconcert/artists/models.py
@@ -5,8 +5,8 @@ from django.core.urlresolvers import reverse
 class Artist(models.Model):
     name = models.CharField(max_length=50)
     bio = models.TextField(max_length=3000)
-    headshot = models.ImageField(upload_to='artist_headshots', blank=True)
-    banner = models.ImageField(upload_to='artist_banners', blank=True)
+    headshot = models.ImageField(upload_to='artist-headshots', blank=True)
+    banner = models.ImageField(upload_to='artist-banners', blank=True)
     slug = models.SlugField(max_length=50, allow_unicode=True, unique=True)
 
     def __str__(self):
